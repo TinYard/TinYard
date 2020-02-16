58c188274d0f9262259a614c31919c2f6ab31981
Kieran Bond <Kieranbond97@gmail.com>
2020-02-16 23:55:09 +0000

    Test and Interface creation for Mapping

---
+++ TinYard Tests/Tests/MappingTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TinYard.API.Interfaces;
+using TinYard.Impl.Exceptions;
+using TinYard.Impl.Mappers;
+using TinYard_Tests.TestClasses;
+
+namespace TinYard.Tests
+{
+    [TestClass]
+    public class MappingTests
+    {
+        private IMapper _mapper;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mapper = new ValueMapper();
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            _mapper = null;
+        }
+
+        [TestMethod]
+        public void ValueMapper_Is_IMapper()
+        {
+            Assert.IsInstanceOfType(_mapper, typeof(IMapper));
+        }
+
+        [TestMethod]
+        public void Mapper_Has_Value_Mapped()
+        {
+            string value = "testval";
+
+            _mapper.Map<string>().ToValue(value);
+
+            string val2 = _mapper.GetValue<string>();
+            Assert.AreEqual(value, val2);
+        }
+    }
+}
---
+++ TinYard/API/Interfaces/IMapper.cs
@@ -0,0 +1,10 @@
+﻿namespace TinYard.API.Interfaces
+{
+    public interface IMapper
+    {
+        IMapper Map<T>();
+        IMapper ToValue(object value);
+
+        T GetValue<T>();
+    }
+}
