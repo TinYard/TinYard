c33a491c9acbe0a4f57a0dd314cb16d7cb5119d2
Kieran Bond <Kieranbond97@gmail.com>
2020-02-16 23:56:05 +0000

    Added first Mapper class type

---
+++ TinYard/Impl/Mappers/ValueMapper.cs
@@ -0,0 +1,22 @@
+﻿using TinYard.API.Interfaces;
+
+namespace TinYard.Impl.Mappers
+{
+    public class ValueMapper : IMapper
+    {
+        public T GetValue<T>()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IMapper Map<T>()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IMapper ToValue(object value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
