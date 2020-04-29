6c0e5626de371b5711d184155e7062e4e40f96da
Kieran Bond <Kieranbond97@gmail.com>
2020-04-29 20:56:31 +0100

    Added impl of EventDispatcher and Listener VO

---
+++ TinYard/Extensions/EventSystem/Impl/VO/Listener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TinYard.Extensions.EventSystem.Impl.VO
+{
+    public class Listener
+    {
+        public Enum @Type { get; private set; }
+        public List<Delegate> ListenerCallbacks { get; private set; }
+
+        public Listener()
+        {
+            ListenerCallbacks = new List<Delegate>();
+        }
+
+        public Listener(Enum type, Delegate listenerCallback)
+        {
+            Type = type;
+            ListenerCallbacks = new List<Delegate>();
+            ListenerCallbacks.Add(listenerCallback);
+        }
+
+        public void AddListener(Delegate listenerCallback)
+        {
+            ListenerCallbacks.Add(listenerCallback);
+        }
+
+        public void RemoveListener(Delegate listenerCallback)
+        {
+            ListenerCallbacks.Remove(listenerCallback);
+        }
+    }
+}
