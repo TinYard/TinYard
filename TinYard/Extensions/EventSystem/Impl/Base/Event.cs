4b4134d2a5ca2924d210ada14e9cf42f20f88c5f
Kieran Bond <Kieranbond97@gmail.com>
2020-04-29 21:26:59 +0100

    Implemented base Event class

---
+++ TinYard/Extensions/EventSystem/Impl/Base/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TinYard.Extensions.EventSystem.API.Interfaces;
+
+namespace TinYard.Extensions.EventSystem.Impl.Base
+{
+    public class Event : IEvent
+    {
+        public Enum type { get { return _type; } }
+        private readonly Enum _type;
+
+        public Event(Enum type)
+        {
+            _type = type;
+        }
+    }
+}
