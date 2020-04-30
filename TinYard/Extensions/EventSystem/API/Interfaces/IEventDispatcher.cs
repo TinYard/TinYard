2676bd59b59bc64105dbed90b6bf31c381051124
Kieran Bond <Kieranbond97@gmail.com>
2020-04-30 11:39:26 +0100

    Added delegate method for add and remove listener

--- TinYard/Extensions/EventSystem/API/Interfaces/IEventDispatcher.cs
+++ TinYard/Extensions/EventSystem/API/Interfaces/IEventDispatcher.cs
@@ -6,9 +6,11 @@ namespace TinYard.Extensions.EventSystem.API.Interfaces
     {
         void AddListener<T>(Enum type, Action<T> listenerCallback);
         void AddListener(Enum type, Action listenerCallback);
+        void AddListener(Enum type, Delegate listenerCallback);
 
         void RemoveListener<T>(Enum type, Action<T> listenerCallback);
         void RemoveListener(Enum type, Action listenerCallback);
+        void RemoveListener(Enum type, Delegate listenerCallback);
 
         void RemoveAllListeners(Enum type);
         void RemoveAllListeners();
