7dc3fef04256732c350e03ae5d10561fb873b66f
Kieran Bond <Kieranbond97@gmail.com>
2020-04-30 11:27:31 +0100

    Added Extension for Event System

---
+++ TinYard/Extensions/EventSystem/EventSystemExtension.cs
@@ -0,0 +1,21 @@
+﻿using TinYard.API.Interfaces;
+using TinYard.Extensions.EventSystem.API.Interfaces;
+using TinYard.Extensions.EventSystem.Impl;
+
+namespace TinYard.Extensions.EventSystem
+{
+    public class EventSystemExtension : IExtension
+    {
+        private IContext _context;
+
+        private IEventDispatcher _eventDispatcher;
+
+        public void Install(IContext context)
+        {
+            _context = context;
+            _eventDispatcher = new EventDispatcher(context);
+
+            _context.Mapper.Map<IEventDispatcher>().ToValue(_eventDispatcher);
+        }
+    }
+}
