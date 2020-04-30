d681017ad24cf019bf6e82a5bd2de0ea1a05e550
Kieran Bond <Kieranbond97@gmail.com>
2020-04-30 22:06:29 +0100

    Added base impl of Injector and tests

---
+++ TinYard Tests/Tests/InjectorTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TinYard.API.Interfaces;
+using TinYard.Framework.API.Interfaces;
+using TinYard.Framework.Impl.Injectors;
+using TinYard_Tests.TestClasses;
+
+namespace TinYard.Tests
+{
+    [TestClass]
+    public class InjectorTests
+    {
+        private IContext _context;
+        private IInjector _injector;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _context = new Context();
+            _injector = new Injector(_context);
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            _context = null;
+            _injector = null;
+        }
+
+        [TestMethod]
+        public void Inject_is_IInjector()
+        {
+            Assert.IsInstanceOfType(_injector, typeof(IInjector));
+        }
+
+        [TestMethod]
+        public void Injector_injects_into_class()
+        {
+            int valueToInject = 5;
+            _context.Mapper.Map<int>().ToValue(valueToInject);
+
+            TestInjectable injectable = new TestInjectable();
+
+            int preInjectValue = injectable.Value;
+            _injector.Inject(injectable);
+
+            //Shouldn't be 0 (default value)
+            Assert.AreNotEqual(preInjectValue, injectable.Value);
+
+            //Should be the value we mapped (5)
+            Assert.AreEqual(injectable.Value, valueToInject);
+        }
+    }
+}
---
+++ TinYard/Framework/Impl/Injectors/Injector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using TinYard.API.Interfaces;
+using TinYard.Framework.API.Interfaces;
+using TinYard.Framework.Impl.Attributes;
+
+namespace TinYard.Framework.Impl.Injectors
+{
+    public class Injector : IInjector
+    {
+        IContext _context;
+        IMapper _mapper;
+
+        public Injector(IContext context)
+        {
+            _context = context;
+            _mapper = _context.Mapper;
+        }
+
+        public void Inject(object classToInjectInto)
+        {
+            //GetType as it's correct at run-time rather than compile time!
+            Type classType = classToInjectInto.GetType();
+
+            List<FieldInfo> injectables = InjectAttribute.GetInjectables(classType);
+
+            foreach(FieldInfo field in injectables)
+            {
+                Type fieldType = field.FieldType;
+                if(_mapper.GetMapping(fieldType) != null)
+                {
+                    field.SetValue(classToInjectInto, _mapper.GetMappingValue(fieldType));
+                }
+            }
+        }
+    }
+}
