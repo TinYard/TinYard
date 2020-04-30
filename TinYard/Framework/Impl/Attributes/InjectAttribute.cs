617380f53917665839d6e55700a419d450a88df1
Kieran Bond <Kieranbond97@gmail.com>
2020-04-30 20:35:00 +0100

    Added InjectAttribute and basic test

---
+++ TinYard/Framework/Impl/Attributes/InjectAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace TinYard.Framework.Impl.Attributes
+{
+    //TODO: See if we can allow injection into Properties.. Might be an issue due to private sets?
+    [AttributeUsage(AttributeTargets.Field /*| AttributeTargets.Property*/, AllowMultiple = false, Inherited = true)]
+    public class InjectAttribute : Attribute
+    {
+        public static List<FieldInfo> GetInjectables(Type classToInjectInto)
+        {
+            List<FieldInfo> injectables = new List<FieldInfo>();
+            
+            foreach(FieldInfo field in classToInjectInto.GetFields())
+            {
+                if (field.GetCustomAttributes<InjectAttribute>(true).Count() > 0)
+                    injectables.Add(field);
+            }
+
+            return injectables;
+        }
+    }
+}
