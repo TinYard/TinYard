01bc5d2a8e2d0a6bb0622be0d342aaaa240cf846
Kieran Bond <Kieranbond97@gmail.com>
2020-05-08 12:07:11 +0100

    Added basic test for extension

---
+++ TinYard Tests/Tests/Extensions/Logging/LoggingExtensionTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TinYard.API.Interfaces;
+using TinYard.Extensions.Logging;
+using TinYard.Impl.Exceptions;
+using TinYard.Tests.MockClasses;
+
+namespace TinYard.Tests.Extensions.Logging
+{
+    [TestClass]
+    public class LoggingExtensionTests
+    {
+        private IContext _context;
+        private LoggingExtension _loggingExtension;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _context = new Context();
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            _context = null;
+        }
+
+        [TestMethod]
+        public void LoggingExtension_is_IExtension()
+        {
+            Assert.IsInstanceOfType(_loggingExtension, typeof(IExtension));
+        }
+
+        [TestMethod]
+        public void Context_Installs_Extension()
+        {
+            _loggingExtension = new LoggingExtension();
+            _context.Install(_loggingExtension);
+            _context.Initialize();
+            //Assert nothing, if it doesn't throw it's a success
+        }
+    }
+}
