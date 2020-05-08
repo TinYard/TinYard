1add94e97554d1f5f4124fa2735ee4403dd206a4
Kieran Bond <Kieranbond97@gmail.com>
2020-05-08 11:58:39 +0100

    Added extension class

---
+++ TinYard/Extensions/Logging/LoggingExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TinYard.API.Interfaces;
+
+namespace TinYard.Extensions.Logging
+{
+    public class LoggingExtension : IExtension
+    {
+        private IContext _context;
+
+        public void Install(IContext context)
+        {
+            _context = context;
+        }
+    }
+}
