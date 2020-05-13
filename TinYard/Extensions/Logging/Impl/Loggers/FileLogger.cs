4fa10a0be02afce0a43a60db8167ec5ef3fa344a
Kieran Bond <Kieranbond97@gmail.com>
2020-05-13 11:52:47 +0100

    Added ILogger interface to FileLogger

--- TinYard/Extensions/Logging/Impl/Loggers/FileLogger.cs
+++ TinYard/Extensions/Logging/Impl/Loggers/FileLogger.cs
@@ -1,6 +1,22 @@
-﻿namespace TinYard.Extensions.Logging.Impl.Loggers
+﻿using TinYard.Extensions.Logging.API.Interfaces;
+
+namespace TinYard.Extensions.Logging.Impl.Loggers
 {
-    public class FileLogger
+    public class FileLogger : ILogger
     {
+        public void Log(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void LogError(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void LogWarning(string message)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
