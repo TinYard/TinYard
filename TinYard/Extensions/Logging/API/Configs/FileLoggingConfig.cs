8acf5aaf3e49bd2cb2ddde93014d934cdf4f0290
Kieran Bond <Kieranbond97@gmail.com>
2020-05-13 12:06:50 +0100

    Added FileLogging config which maps the logger

---
+++ TinYard/Extensions/Logging/API/Configs/FileLoggingConfig.cs
@@ -0,0 +1,18 @@
+﻿using TinYard.API.Interfaces;
+using TinYard.Extensions.Logging.API.Interfaces;
+using TinYard.Extensions.Logging.Impl.Loggers;
+using TinYard.Framework.Impl.Attributes;
+
+namespace TinYard.Extensions.Logging.API.Configs
+{
+    public class FileLoggingConfig : IConfig
+    {
+        [Inject]
+        private IMapper _contextMapper;
+
+        public void Configure()
+        {
+            _contextMapper.Map<ILogger>().ToValue(new FileLogger());
+        }
+    }
+}
