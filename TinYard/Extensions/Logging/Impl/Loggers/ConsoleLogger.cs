c1b5464f291407b863e51ed6c64e6d8d5656d279
Kieran Bond <Kieranbond97@gmail.com>
2020-06-02 14:50:33 +0100

    Added Console Logging config and class

---
+++ TinYard/Extensions/Logging/Impl/Loggers/ConsoleLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TinYard.Extensions.Logging.API.Interfaces;
+
+namespace TinYard.Extensions.Logging.Impl.Loggers
+{
+    public class ConsoleLogger : ILogger
+    {
+        public string LastLogFilePath => throw new NotImplementedException();
+
+        public void Log(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogWarning(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
