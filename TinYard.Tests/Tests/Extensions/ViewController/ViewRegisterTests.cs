fb33e056e5decafa17369c02df9d4a1d57a5e716
Kieran Bond <Kieranbond97@gmail.com>
2020-06-19 08:51:29 +0100

    Added more tests for ViewRegister
    Added TestView class

--- TinYard.Tests/Tests/Extensions/ViewController/ViewRegisterTests.cs
+++ TinYard.Tests/Tests/Extensions/ViewController/ViewRegisterTests.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using TinYard.API.Interfaces;
 using TinYard.Extensions.ViewController.API.Interfaces;
 using TinYard.Extensions.ViewController.Impl.Base;
+using TinYard.Extensions.ViewController.Tests.MockClasses;
 
 namespace TinYard.Extensions.ViewController.Tests
 {
@@ -28,6 +32,35 @@ namespace TinYard.Extensions.ViewController.Tests
         public void ViewRegister_is_IViewRegister()
         {
             Assert.IsInstanceOfType(_register, typeof(IViewRegister));
+        }
+
+        [TestMethod]
+        public void ViewRegister_Instance_Is_Created()
+        {
+            Assert.IsNotNull(ViewRegister.Instance);
+        }
+
+        [TestMethod]
+        public void IView_Is_Registered()
+        {
+            TestView expected = new TestView();
+            ViewRegister.Register(expected);
+
+            IReadOnlyList<IView> registeredViews = ViewRegister.Instance.RegisteredViews;
+            
+            Assert.IsTrue(registeredViews.Contains(expected));
+        }
+
+        [TestMethod]
+        public void IView_Cant_Register_Twice()
+        {
+            TestView testView = new TestView();
+
+            bool actual1 = ViewRegister.Register(testView);
+            bool actual2 = ViewRegister.Register(testView);
+
+            Assert.IsTrue(actual1);
+            Assert.IsFalse(actual2);
         }
     }
 }
