17422698911290330b78707088fe2fb65d195c5d
Kieran Bond <Kieranbond97@gmail.com>
2020-06-20 22:18:22 +0100

    Added build functionality, added IMapper dependency

--- TinYard/Framework/Impl/Factories/MappingValueFactory.cs
+++ TinYard/Framework/Impl/Factories/MappingValueFactory.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using TinYard.API.Interfaces;
 using TinYard.Framework.API.Interfaces;
 
 namespace TinYard.Framework.Impl.Factories
 {
     public class MappingValueFactory<T> : IMappingFactory<T>
     {
+        private IMapper _mapper;
+        private Type _type;
+
+        public MappingValueFactory(IMapper mapper)
+        {
+            _mapper = mapper;
+            _type = typeof(T);
+        }
+
         public T Build()
         {
-            throw new NotImplementedException();
+            T value = (T)Activator.CreateInstance(_type);
+            return value;
         }
     }
 }
