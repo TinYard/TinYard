2a3a25f3b2a004c5643e9883374be800f5ef881d
Kieran Bond <Kieranbond97@gmail.com>
2020-06-20 22:36:26 +0100

    Added more advanced build impl

--- TinYard/Framework/Impl/Factories/MappingValueFactory.cs
+++ TinYard/Framework/Impl/Factories/MappingValueFactory.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using TinYard.API.Interfaces;
 using TinYard.Framework.API.Interfaces;
 
@@ -17,8 +20,34 @@ namespace TinYard.Framework.Impl.Factories
 
         public T Build()
         {
-            T value = (T)Activator.CreateInstance(_type);
+            object[] constructorDependencies = GetConstructorDependencies();
+
+            //If we have any null objects, we can break something further down the line.
+            //Making the array null means we should use the default constructor instead when possible
+            if (constructorDependencies.Any().GetType() == null)
+                constructorDependencies = null;
+
+            T value = (T)Activator.CreateInstance(_type, constructorDependencies);
             return value;
+        }
+
+        private object[] GetConstructorDependencies()
+        {
+            ParameterInfo[] constructorInfo = _type.GetConstructors().FirstOrDefault().GetParameters();
+
+            List<object> constructorParams = new List<object>();
+            foreach(ParameterInfo constructorParameter in constructorInfo)
+            {
+                object value = GetValueFromMapper(constructorParameter.ParameterType);
+                constructorParams.Add(value);
+            }
+
+            return constructorParams.ToArray();
+        }
+
+        private object GetValueFromMapper(Type parameterType)
+        {
+            return _mapper.GetMappingValue(parameterType);
         }
     }
 }
