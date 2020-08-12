b1386838d99a5cc675da9de9fade8c5c313d83f8
Kieran Bond <Kieranbond97@gmail.com>
2020-08-12 15:01:59 +0100

    Update MediatorMapper to be type IMapper

--- TinYard/Extensions/MediatorMap/Impl/Mappers/MediatorMapper.cs
+++ TinYard/Extensions/MediatorMap/Impl/Mappers/MediatorMapper.cs
@@ -1,13 +1,51 @@
-﻿using TinYard.Extensions.MediatorMap.API.Interfaces;
+﻿using System;
+using TinYard.API.Interfaces;
+using TinYard.Extensions.MediatorMap.API.Interfaces;
+using TinYard.Extensions.MediatorMap.Impl.VO;
+using TinYard.Framework.API.Interfaces;
 using TinYard.Impl.Mappers;
 
 namespace TinYard.Extensions.MediatorMap.Impl.Mappers
 {
-    public class MediatorMapper : ValueMapper, IMediatorMapper
+    public class MediatorMapper : IMapper<IMediatorMappingObject>, IMediatorMapper
     {
         public MediatorMapper()
         {
             //_mappingFactory = new MediatorFactory(this);
+        }
+
+        public IMappingFactory MappingFactory => throw new NotImplementedException();
+
+        public event Action<IMediatorMappingObject> OnValueMapped;
+
+        public System.Collections.Generic.IReadOnlyList<IMediatorMappingObject> GetAllMappings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMediatorMappingObject GetMapping<T2>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMediatorMappingObject GetMapping(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetMappingValue<T2>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetMappingValue(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMediatorMappingObject Map<T2>(bool autoInitializeValue = false)
+        {
+            throw new NotImplementedException();
         }
     }
 }
