a5b772d649a548f5831671b074bc1bedc4c10385
Kieran Bond <Kieranbond97@gmail.com>
2020-08-20 11:10:43 +0100

    Add base Mediator and IMediator interface
    Add missing test class

---
+++ TinYard.Tests/TestClasses/TestMediator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TinYard.Extensions.MediatorMap.API.Base;
+
+namespace TinYard.Tests.TestClasses
+{
+    public class TestMediator : Mediator
+    {
+        public TestMediator()
+        {
+        }
+    }
+}
---
+++ TinYard/Extensions/MediatorMap/API/Base/Mediator.cs
@@ -0,0 +1,10 @@
+﻿using TinYard.Extensions.EventSystem.Impl;
+using TinYard.Extensions.MediatorMap.API.Interfaces;
+
+namespace TinYard.Extensions.MediatorMap.API.Base
+{
+    public class Mediator : EventDispatcher, IMediator
+    {
+       
+    }
+}
