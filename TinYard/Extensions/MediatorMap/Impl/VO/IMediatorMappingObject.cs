79b867f2324e503616a19efdbc46241617f06c6f
Kieran Bond <Kieranbond97@gmail.com>
2020-08-20 11:11:13 +0100

    Add missing IMediatorMappingObject and impl

---
+++ TinYard/Extensions/MediatorMap/Impl/VO/IMediatorMappingObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TinYard.Extensions.MediatorMap.API.Interfaces;
+using TinYard.Extensions.ViewController.API.Interfaces;
+
+namespace TinYard.Extensions.MediatorMap.Impl.VO
+{
+    public interface IMediatorMappingObject
+    {
+        Type View { get; }
+        Type Mediator { get; }
+
+        event Action<IMediatorMappingObject> OnViewMapped;
+
+        IMediatorMappingObject Map<T>();
+
+        IMediatorMappingObject ToMediator<T>() where T : IMediator;
+        IMediatorMappingObject ToMediator(IMediator mediator);
+    }
+}
---
+++ TinYard/Extensions/MediatorMap/Impl/VO/MediatorMappingObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using TinYard.Extensions.MediatorMap.API.Interfaces;
+using TinYard.Extensions.ViewController.API.Interfaces;
+
+namespace TinYard.Extensions.MediatorMap.Impl.VO
+{
+    public class MediatorMappingObject : IMediatorMappingObject
+    {
+        public Type View { get; private set; }
+
+        public Type Mediator { get; private set; }
+
+        public event Action<IMediatorMappingObject> OnViewMapped;
+
+        public IMediatorMappingObject Map<T>()
+        {
+            View = typeof(T);
+
+            return this;
+        }
+
+        public IMediatorMappingObject ToMediator<T>() where T : IMediator
+        {
+            Mediator = typeof(T);
+
+            return this;
+        }
+
+        public IMediatorMappingObject ToMediator(IMediator mediator)
+        {
+            Mediator = mediator.GetType();
+
+            return this;
+        }
+    }
+}
