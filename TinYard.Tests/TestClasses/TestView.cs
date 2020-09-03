cebc18a8e0fb359d5a60d6844009cd952b1bb86b
Kieran Bond <Kieranbond97@gmail.com>
2020-09-03 17:39:30 +0100

    Add register to testview

--- TinYard.Tests/TestClasses/TestView.cs
+++ TinYard.Tests/TestClasses/TestView.cs
@@ -1,9 +1,14 @@
 ﻿using TinYard.Extensions.ViewController.API.Interfaces;
+using TinYard.Extensions.ViewController.Impl.Base;
 
 namespace TinYard.Extensions.ViewController.Tests.MockClasses
 {
     public class TestView : IView
     {
-
+        public TestView()
+        {
+            //Register if possible
+            ViewRegister.Register(this);
+        }
     }
 }
