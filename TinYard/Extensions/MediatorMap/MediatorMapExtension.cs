2a0d747c488d73531edbd1e13cd86a447d9e5c9b
Kieran Bond <Kieranbond97@gmail.com>
2020-09-03 16:38:32 +0100

    Modify MediatorMapExtension map `MediatorMapper`
    Now mapped post Context config install so that we can later get needed pieces

--- TinYard/Extensions/MediatorMap/MediatorMapExtension.cs
+++ TinYard/Extensions/MediatorMap/MediatorMapExtension.cs
@@ -1,14 +1,33 @@
-﻿using TinYard.API.Interfaces;
+﻿using System;
+using TinYard.API.Interfaces;
 using TinYard.Extensions.MediatorMap.API.Interfaces;
 using TinYard.Extensions.MediatorMap.Impl.Mappers;
+using TinYard.Extensions.ViewController.API.Interfaces;
+using TinYard.Framework.API.Interfaces;
 
 namespace TinYard.Extensions.MediatorMap
 {
     public class MediatorMapExtension : IExtension
     {
+        private IContext _context;
+
         public void Install(IContext context)
         {
-            context.Mapper.Map<IMediatorMapper>().ToValue(new MediatorMapper());
+            _context = context;
+
+            _context.PostConfigsInstalled += OnContextInitialized;
+        }
+
+        private void OnContextInitialized()
+        {
+            //var viewRegister = _context.Mapper.GetMappingValue<IViewRegister>() as IViewRegister;
+            //var injector = _context.Mapper.GetMappingValue<IInjector>() as IInjector;
+
+            //if (viewRegister == null || injector == null)
+            //    return;
+
+            var mediatorMapper = new MediatorMapper();
+            _context.Mapper.Map<IMediatorMapper>().ToValue(mediatorMapper);
         }
     }
 }
