d1e31058b0346b4a0ef9567d47a04186070f8737
Kieran Bond <Kieranbond97@gmail.com>
2020-09-03 16:19:08 +0100

    Modify TestMediator for better test capabilities

--- TinYard.Tests/TestClasses/TestMediator.cs
+++ TinYard.Tests/TestClasses/TestMediator.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using TinYard.Extensions.MediatorMap.API.Base;
+﻿using TinYard.Extensions.MediatorMap.API.Base;
+using TinYard.Extensions.ViewController.Tests.MockClasses;
+using TinYard.Framework.Impl.Attributes;
 
 namespace TinYard.Tests.TestClasses
 {
     public class TestMediator : Mediator
     {
+        [Inject]
+        public TestView View;
+
         public TestMediator()
+        {
+        }
+
+        public override void Configure()
         {
         }
     }
