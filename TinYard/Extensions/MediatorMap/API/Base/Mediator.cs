f6b7d8ffa811ea882ca53c4ac75ac51d905e80e8
Kieran Bond <Kieranbond97@gmail.com>
2020-09-04 10:55:29 +0100

    Add ViewComponent and viewDispatcher to Mediator base
    ViewComponent tries to fetch viewDispatcher when set from view

--- TinYard/Extensions/MediatorMap/API/Base/Mediator.cs
+++ TinYard/Extensions/MediatorMap/API/Base/Mediator.cs
@@ -1,5 +1,8 @@
-﻿using TinYard.Extensions.EventSystem.API.Interfaces;
+﻿using System;
+using System.Reflection;
+using TinYard.Extensions.EventSystem.API.Interfaces;
 using TinYard.Extensions.MediatorMap.API.Interfaces;
+using TinYard.Extensions.ViewController.API.Interfaces;
 using TinYard.Framework.Impl.Attributes;
 
 namespace TinYard.Extensions.MediatorMap.API.Base
@@ -9,9 +12,73 @@ namespace TinYard.Extensions.MediatorMap.API.Base
         [Inject]
         public IEventDispatcher Dispatcher;
 
+        public IView ViewComponent
+        {
+            get
+            {
+                return _view;
+            }
+            set 
+            { 
+                _view = value;
+                _viewDispatcher = GetDispatcher(_view);
+            }
+        }
+
+        private IView _view;
+        private IEventDispatcher _viewDispatcher;
+
         /// <summary>
         /// Add your listeners here. This will be called once the Mediator has been injected into
         /// </summary>
         public abstract void Configure();
+
+        private IEventDispatcher GetDispatcher(object dispatcherContainer)
+        {
+            if (dispatcherContainer == null)
+                return null;
+
+            if (dispatcherContainer is IEventDispatcher)
+                return dispatcherContainer as IEventDispatcher;
+
+            Type dispatcherContainerType = dispatcherContainer.GetType();
+
+            PropertyInfo[] properties = dispatcherContainerType.GetProperties
+                (
+                    BindingFlags.Public |
+                    BindingFlags.Instance |
+                    BindingFlags.FlattenHierarchy
+                );
+
+            foreach(PropertyInfo property in properties)
+            {
+                var propertyValue = property.GetValue(dispatcherContainer);
+                if(propertyValue is IEventDispatcher)
+                {
+                    return propertyValue as IEventDispatcher;
+                }
+            }
+
+            //No properties of that type..
+            //Try Fields
+            FieldInfo[] fields = dispatcherContainerType.GetFields
+                (
+                    BindingFlags.Public |
+                    BindingFlags.Instance |
+                    BindingFlags.FlattenHierarchy
+                );
+
+            foreach (FieldInfo field in fields)
+            {
+                var fieldValue = field.GetValue(dispatcherContainer);
+                if(fieldValue is IEventDispatcher)
+                {
+                    return fieldValue as IEventDispatcher;
+                }
+            }
+
+            //Seems there is no IEventDispatcher!
+            return null;
+        }
     }
 }
