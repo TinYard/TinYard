6451f007250fba6d9a40c2678123cd90d463d8f6
Kieran Bond <Kieranbond97@gmail.com>
2020-09-07 16:16:47 +0100

    Add folder struct and ICommand interface

---
+++ TinYard/Extensions/CommandSystem/CommandSystemExtension.cs
@@ -0,0 +1,12 @@
+﻿using TinYard.API.Interfaces;
+
+namespace TinYard.Extensions.CommandSystem
+{
+    public class CommandSystemExtension : IExtension
+    {
+        public void Install(IContext context)
+        {
+            
+        }
+    }
+}
