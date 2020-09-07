ea65a4f94e89b1550ee8e6e07848b9d98ec5a052
Kieran Bond <Kieranbond97@gmail.com>
2020-09-07 17:02:54 +0100

    Add ICommandMap and ICommandMapping interfaces

---
+++ TinYard/Extensions/CommandSystem/API/Interfaces/ICommandMap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using TinYard.Extensions.EventSystem.API.Interfaces;
+
+namespace TinYard.Extensions.CommandSystem.API.Interfaces
+{
+    public interface ICommandMap
+    {
+        ICommandMapping Map<T>() where T : IEvent;
+        ICommandMapping Map<T>(Enum type) where T : IEvent;
+    }
+}
---
+++ TinYard/Extensions/CommandSystem/API/Interfaces/ICommandMapping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TinYard.Extensions.EventSystem.API.Interfaces;
+
+namespace TinYard.Extensions.CommandSystem.API.Interfaces
+{
+    public interface ICommandMapping
+    {
+        Type Event { get; }
+        Enum EventType { get; }
+
+        Type Command { get; }
+
+        ICommandMapping Map<T>(Enum type = null) where T : IEvent;
+
+        ICommandMapping ToCommand<T>() where T : ICommand;
+    }
+}
