83e16539484cbeb3c498b9fa25c3ddbe61757955
Kieran Bond <Kieranbond97@gmail.com>
2020-09-08 09:01:06 +0100

    Modify CommandSystemExtension to map EventCommandMap

--- TinYard/Extensions/CommandSystem/CommandSystemExtension.cs
+++ TinYard/Extensions/CommandSystem/CommandSystemExtension.cs
@@ -1,12 +1,33 @@
-﻿using TinYard.API.Interfaces;
+﻿using System;
+using TinYard.API.Interfaces;
+using TinYard.Extensions.CommandSystem.API.Interfaces;
+using TinYard.Extensions.CommandSystem.Impl.CommandMaps;
+using TinYard.Extensions.EventSystem.API.Interfaces;
 
 namespace TinYard.Extensions.CommandSystem
 {
     public class CommandSystemExtension : IExtension
     {
+        private IContext _context;
+
         public void Install(IContext context)
         {
-            
+            _context = context;
+            _context.PostConfigsInstalled += PostConfigsInstalled;
+        }
+
+        private void PostConfigsInstalled()
+        {
+            var eventDispatcher = _context.Mapper.GetMappingValue<IEventDispatcher>() as IEventDispatcher;
+            var injector = _context.Injector;
+
+            if(eventDispatcher == null || injector == null)
+            {
+                throw new NullReferenceException("Event Dispatcher or Injector are null in CommandSystemExtension");
+            }
+
+            EventCommandMap commandMap = new EventCommandMap(eventDispatcher, injector);
+            _context.Mapper.Map<ICommandMap>().ToValue(commandMap);
         }
     }
 }
