9393671d56cb23323d5946af46351cfc1e67fc7d
Kieran Bond <Kieranbond97@gmail.com>
2020-09-08 08:51:51 +0100

    Add ICommandFactory interface

---
+++ TinYard/Extensions/CommandSystem/API/Interfaces/ICommandFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using TinYard.Framework.API.Interfaces;
+
+namespace TinYard.Extensions.CommandSystem.API.Interfaces
+{
+    public interface ICommandFactory : IFactory
+    {
+        ICommand Build<T>() where T : ICommand;
+        ICommand Build(Type commandType);
+    }
+}
