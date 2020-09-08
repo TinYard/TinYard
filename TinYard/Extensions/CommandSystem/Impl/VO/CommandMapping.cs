b7913356f97c643b187274a51c5fdb668e9c843f
Kieran Bond <Kieranbond97@gmail.com>
2020-09-08 08:51:23 +0100

    Add CommandMapping

---
+++ TinYard/Extensions/CommandSystem/Impl/VO/CommandMapping.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TinYard.Extensions.CommandSystem.API.Interfaces;
+using TinYard.Extensions.EventSystem.API.Interfaces;
+
+namespace TinYard.Extensions.CommandSystem.Impl.VO
+{
+    public class CommandMapping : ICommandMapping
+    {
+        public Type Event { get; private set; }
+
+        public Enum EventType { get; private set; }
+
+        public Type Command { get; private set; }
+
+        public ICommandMapping Map<T>(Enum type = null) where T : IEvent
+        {
+            Event = typeof(T);
+            EventType = type;
+
+            return this;
+        }
+
+        public ICommandMapping ToCommand<T>() where T : ICommand
+        {
+            Command = typeof(T);
+
+            return this;
+        }
+    }
+}
