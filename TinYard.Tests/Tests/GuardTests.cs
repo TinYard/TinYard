b3d05be4506190e2f1701463f02561554c39b97a
Kieran Bond <Kieranbond97@gmail.com>
2020-10-04 16:04:01 +0100

    Add Guard not satisfied blocks command test
    Modified tests by tidying test with helper method

--- TinYard.Tests/Tests/GuardTests.cs
+++ TinYard.Tests/Tests/GuardTests.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using TinYard.API.Interfaces;
 using TinYard.Extensions.CommandSystem;
 using TinYard.Extensions.CommandSystem.API.Interfaces;
 using TinYard.Extensions.EventSystem;
+using TinYard.Extensions.EventSystem.API.Interfaces;
 using TinYard.Extensions.EventSystem.Tests.MockClasses;
 using TinYard.Tests.TestClasses;
 
@@ -13,6 +15,7 @@ namespace TinYard.Tests
     {
         private IContext _context;
         private ICommandMap _commandMap;
+        private IEventDispatcher _eventDispatcher;
 
         [TestInitialize]
         public void Setup()
@@ -30,13 +33,33 @@ namespace TinYard.Tests
         [TestMethod]
         public void Guard_Can_Be_Added_To_Command()
         {
+            SetupCommandExtension();
+
+            _commandMap.Map<TestEvent>(TestEvent.Type.Test1).ToCommand<TestCommand>().WithGuard<TestGuard>();
+        }
+
+        [TestMethod]
+        public void Command_Doesnt_Run_If_Guard_Not_Satisfied()
+        {
+            SetupCommandExtension();
+
+            _commandMap.Map<TestEvent>(TestEvent.Type.Test1).ToCommand<TestCommand>().WithGuard<TestGuard>();
+
+            Assert.IsFalse(TestCommand.ExecuteCalled);
+
+            _eventDispatcher.Dispatch(new TestEvent(TestEvent.Type.Test1));
+
+            Assert.IsFalse(TestCommand.ExecuteCalled);
+        }
+
+        private void SetupCommandExtension()
+        {
             _context.Install(new EventSystemExtension());
             _context.Install(new CommandSystemExtension());
             _context.Initialize();
 
             _commandMap = _context.Mapper.GetMappingValue<ICommandMap>() as ICommandMap;
-
-            _commandMap.Map<TestEvent>(TestEvent.Type.Test1).ToCommand<TestCommand>().WithGuard<TestGuard>();
+            _eventDispatcher = _context.Mapper.GetMappingValue<IEventDispatcher>() as IEventDispatcher;
         }
     }
 }
