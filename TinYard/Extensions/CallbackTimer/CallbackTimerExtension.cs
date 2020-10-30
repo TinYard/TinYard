39419b795d79950033a6dba21ad51e400631a062
Kieran Bond <Kieranbond97@gmail.com>
2020-10-30 17:10:52 +0000

    Add extension

---
+++ TinYard/Extensions/CallbackTimer/CallbackTimerExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TinYard.API.Interfaces;
+
+namespace TinYard.Extensions.CallbackTimer
+{
+    public class CallbackTimerExtension : IExtension
+    {
+        public void Install(IContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
