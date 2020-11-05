54dc565bbf269f64e38f337767a19e0ba736c776
Kieran Bond <Kieranbond97@gmail.com>
2020-11-05 10:45:18 +0000

    Add forgot Timer Class!

---
+++ TinYard/Extensions/CallbackTimer/Impl/VO/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TinYard.Extensions.CallbackTimer.Impl.VO
+{
+    public class Timer
+    {
+        public Action TimerCallback { get { return _timerCallback; } }
+        private Action _timerCallback;
+
+        public double TimerDuration { get { return _timerDuration; } }
+        private double _timerDuration;
+
+        public double CurrentLifetime { get { return _currentLifetime; } }
+        private double _currentLifetime;
+
+        private bool _timerFinished = false;
+
+        public Timer(double durationInSeconds, Action timerCallback)
+        {
+            _timerDuration = durationInSeconds;
+            _currentLifetime = 0d;
+
+            _timerCallback = timerCallback;
+        }
+
+        public void Update(double deltaTime)
+        {
+            _currentLifetime += deltaTime;
+
+            if (!_timerFinished && _currentLifetime >= TimerDuration)
+            {
+                _timerFinished = true;
+
+                _timerCallback?.Invoke();
+            }
+        }
+    }
+}
