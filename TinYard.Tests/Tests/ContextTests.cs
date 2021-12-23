fcee52b0f0b8fb6988f40c5ca697f2012bcdb164
Kieran Bond <kieranbond97@gmail.com>
2021-12-23 14:15:22 +0000

    Revert unusued usings

--- TinYard.Tests/Tests/ContextTests.cs
+++ TinYard.Tests/Tests/ContextTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Logging.Abstractions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using TinYard.API.Interfaces;
 using TinYard.Framework.API.Interfaces;
